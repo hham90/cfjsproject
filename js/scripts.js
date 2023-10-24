1b337b05b8a621aa5b277447ca3e04a87b42a52e
hham90 <148152147+hham90@users.noreply.github.com>
2023-10-24 15:48:30 -0400

    added list of pokemon
    included name, height and, type

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-alert('Hello World');
-let favoriteFood = 'Spicy Pork Belly'
-document.write(favoriteFood)
+let pokemonList = [
+  {name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
+  {name: 'Charmander', height: .6, types: ['fire']},
+  {name: 'Squirtle', height: .5, types: ['water']},
+  {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+  {name: 'Venasaur', height: 2, types: ['grass', 'poison']},
+  {name: 'Charmeleon', height: 1.1, types: ['fire']},
+  {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+  {name: 'Wartortle', height: 1, types: ['water']},
+  {name: 'Blastoise', height: 1.6, types: ['water']},
+];
