1cfa682306b1c5a7335bce204c163f5c2edfe7e0
hham90 <148152147+hham90@users.noreply.github.com>
2023-10-25 20:18:34 -0400

    updated scripts
    added loop
    added conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 let pokemonList = [
-  {name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
-  {name: 'Charmander', height: .6, types: ['fire']},
-  {name: 'Squirtle', height: .5, types: ['water']},
-  {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-  {name: 'Venasaur', height: 2, types: ['grass', 'poison']},
-  {name: 'Charmeleon', height: 1.1, types: ['fire']},
-  {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-  {name: 'Wartortle', height: 1, types: ['water']},
-  {name: 'Blastoise', height: 1.6, types: ['water']},
-];
+    {name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
+    {name: 'Charmander', height: .6, types: ['fire']},
+    {name: 'Squirtle', height: .5, types: ['water']},
+    {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+    {name: 'Venasaur', height: 2, types: ['grass', 'poison']},
+    {name: 'Charmeleon', height: 1.1, types: ['fire']},
+    {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+    {name: 'Wartortle', height: 1, types: ['water']},
+    {name: 'Blastoise', height: 1.6, types: ['water']},
+  ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")");
+    if (pokemonList[i].height > 1.1) {
+      document.write(" Wow, that\’s big!");
+    }
+    document.write("<br/>");
+}
+
+
